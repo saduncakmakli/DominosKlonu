7fb35faa370b97cf16d6c8ae96db77b7f7f3a69b
Muhammed Sadun Çakmaklı <muhammedsaduncakmakli@gmail.com>
2022-02-27 06:11:15 +0300

    Kampanya RecycleView created on FragmentHome.

--- app/src/main/java/com/example/dominosklonu/fragment/FragmentAnasayfa.kt
+++ app/src/main/java/com/example/dominosklonu/fragment/FragmentAnasayfa.kt
@@ -11,9 +11,11 @@ import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.example.dominosklonu.adapter.adaptersData.Pizza
 import com.example.dominosklonu.R
+import com.example.dominosklonu.adapter.KampanyaAdapter
 import com.example.dominosklonu.adapter.adaptersData.SliderItem
 import com.example.dominosklonu.adapter.PizzaAdapter
 import com.example.dominosklonu.adapter.SliderAdapter
+import com.example.dominosklonu.adapter.adaptersData.Kampanya
 import com.example.dominosklonu.databinding.FragmentAnasayfaBinding
 
 class FragmentAnasayfa : Fragment() {
@@ -21,10 +23,6 @@ class FragmentAnasayfa : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         tasarim = DataBindingUtil.inflate(inflater, R.layout.fragment_anasayfa, container, false)
         tasarim.anasayfaFragment = this
-        tasarim.rvSlider.layoutManager = StaggeredGridLayoutManager(1, StaggeredGridLayoutManager.HORIZONTAL)
-
-
-
 
         val pizzaListesi = ArrayList<Pizza>()
         val p1 = Pizza(1, "Gel Al - 2 Büyük Boy Pizza + Coca-Cola Pet 1 lt", "gel_al_2_buyuk_boy_pizza_coca_cola_pet_1_lt", 86.99)
@@ -38,25 +36,62 @@ class FragmentAnasayfa : Fragment() {
         val adapter = PizzaAdapter(requireContext(),pizzaListesi)
         tasarim.pizzaAdapter = adapter
 
-        val kampanyaListesi = ArrayList<SliderItem>()
-        val k1 = SliderItem(1, "kampanya1")
-        val k2 = SliderItem(2, "kampanya2")
-        val k3 = SliderItem(3, "kampanya3")
+        tasarim.rvSlider.layoutManager = StaggeredGridLayoutManager(1, StaggeredGridLayoutManager.HORIZONTAL) //RecycleView Setting
+        val sliderListesi = ArrayList<SliderItem>()
+        val s1 = SliderItem(1, "kampanya1")
+        val s2 = SliderItem(2, "kampanya2")
+        val s3 = SliderItem(3, "kampanya3")
+
+        sliderListesi.add(s1)
+        sliderListesi.add(s2)
+        sliderListesi.add(s3)
+
+        val sliderAdapter = SliderAdapter(requireContext(),sliderListesi)
+        tasarim.sliderAdapter = sliderAdapter
+
+        tasarim.rvKampanya.layoutManager = StaggeredGridLayoutManager(1, StaggeredGridLayoutManager.HORIZONTAL) //RecycleView Setting
+        val kampanyaListesi = ArrayList<Kampanya>()
+        val k1 = Kampanya(1, "Gel Al Haftanın Kampanyaları")
+        val k2 = Kampanya(2,"Beklemeden Gel Al Özel")
+        val k3 = Kampanya(3, "Gel Al 40 TL Altı Lezzetler")
+        val k4 = Kampanya(4,"Gel Al Bol Lezzetler")
+        val k5 = Kampanya(5, "Gel Al Tek Kişilik")
+        val k6 = Kampanya(6, "Gel Al 2-3 Kişilik")
+        val k7 = Kampanya(7, "Gel Al 4 Kişi ve Üzeri")
+        val k8 = Kampanya(8, "Uygulamaya Özel Fırsatlar")
+        val k9 = Kampanya(9, "Haftanın Kampanyaları")
+        val k10 = Kampanya(10, "50 TL Altı Lezzetler")
+        val k11 = Kampanya(11, "Bol Lezzetler")
+        val k12 = Kampanya(12, "Tek Kişilik Kampanyalar")
+        val k13 = Kampanya(13, "2-3 Kişilik Kampanyalar")
+        val k14 = Kampanya(14, "4 Kişi ve Üzeri Kampanyalar")
 
         kampanyaListesi.add(k1)
         kampanyaListesi.add(k2)
         kampanyaListesi.add(k3)
+        kampanyaListesi.add(k4)
+        kampanyaListesi.add(k5)
+        kampanyaListesi.add(k6)
+        kampanyaListesi.add(k7)
+        kampanyaListesi.add(k8)
+        kampanyaListesi.add(k9)
+        kampanyaListesi.add(k10)
+        kampanyaListesi.add(k11)
+        kampanyaListesi.add(k12)
+        kampanyaListesi.add(k13)
+        kampanyaListesi.add(k14)
 
-        val kampanyaAdapter = SliderAdapter(requireContext(),kampanyaListesi)
+        val kampanyaAdapter = KampanyaAdapter(requireContext(), kampanyaListesi)
         tasarim.kampanyaAdapter = kampanyaAdapter
 
+        //Slider Oto-Slide
         var position = 0
         var firstOpen = true
         val countDownInterval:Long = 5000
-        val timer = object: CountDownTimer((countDownInterval*kampanyaListesi.size*5+2), countDownInterval) {
+        val timer = object: CountDownTimer((countDownInterval*sliderListesi.size*5+2), countDownInterval) {
             override fun onTick(millisUntilFinished: Long) {
                 if (!firstOpen){
-                    if (position++ < kampanyaListesi.size-1){ }
+                    if (position++ < sliderListesi.size-1){ }
                     else {
                         position = 0
                     }
